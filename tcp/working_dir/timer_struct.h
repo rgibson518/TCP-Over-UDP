418cfc35cd531845327dae55c03a3fc0853cb80d
rgibson518 <gibsonr@cse.osu.edu>
2014-04-15 14:52:32 -0400

    Delete timer_struct.h

--- tcp/working_dir/timer_struct.h
+++
@@ -1,7 +0,0 @@
-typedef struct timer
-{
-	unsigned long duration; //duration of timer in microseconds
-	unsigned int packet_number; //sequence number of corresponding packet
-	unsigned int port_number; //number of the corresponding port
-	struct timer *next;
-} timer;
